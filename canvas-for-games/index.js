7a607c081c87a565c5302638daeaea08e57bc0bb
Daniel Yoo <jarkassp2@gmail.com>
2024-11-09 23:50:05 -0500

    save from the last moment

--- canvas-for-games/index.js
+++ canvas-for-games/index.js
@@ -1,11 +1,39 @@
+class Component {
+  x = 0;
+  y = 0;
+  dx = 0;
+  dy = 0;
+  width = 0;
+  height = 0;
+  color = "";
+
+  constructor(x, y, dx, dy, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw(context) {
+    context.fillStyle = this.color;
+    this.x += this.dx;
+    this.y += this.dy;
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  clear(context) {
+    context.clearRect(this.x, this.y, this.width, this.height);
+  }
+}
+
 let context;
 
-let x = 100;
-let y = 100;
-let dx = 0; // speed on x-axis
-let dy = 0; // speed on y-axis
-let width = 25;
-let height = 25;
+const main = new Component(100, 100, 0, 0, 25, 25, "green");
+const obstacle = new Component(400, 0, -1, 0, 10, 200, "red");
+const obstacle2 = new Component(800, 600, -2, 0, 10, 200, "red");
 
 function startGame() {
   const canvas = document.getElementById("my-canvas");
@@ -16,17 +44,18 @@ function startGame() {
 }
 
 function draw() {
-  context.fillStyle = "green";
-  x += dx;
-  y += dy;
-  context.fillRect(x, y, width, height);
+  main.draw(context);
+  obstacle.draw(context);
+  obstacle2.draw(context);
   document.getElementById(
     "box-status"
-  ).textContent = `x: ${x}, y: ${y}, dx: ${dx}, dy: ${dy}`;
+  ).textContent = `x: ${main.x}, y: ${main.y}, dx: ${main.dx}, dy: ${main.dy}`;
 }
 
 function clear() {
-  context.clearRect(x, y, width, height);
+  main.clear(context);
+  obstacle.clear(context);
+  obstacle2.clear(context);
 }
 
 function render() {
@@ -39,16 +68,16 @@ function render() {
 window.addEventListener("keydown", function (event) {
   switch (event.key) {
     case "ArrowUp":
-      dy -= 1;
+      main.dy -= 1;
       break;
     case "ArrowDown":
-      dy += 1;
+      main.dy += 1;
       break;
     case "ArrowLeft": // <-
-      dx -= 1;
+      main.dx -= 1;
       break;
     case "ArrowRight": // ->
-      dx += 1;
+      main.dx += 1;
       break;
   }
 });
